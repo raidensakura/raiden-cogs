1953070a7f7bc6d69ff57cb9bade34fa142ec5f5
Raiden <raidensakurajima@gmail.com>
2023-01-26 22:44:00 +0800

    Add dictionary

---
+++ genshinutils/constants.py
@@ -0,0 +1,13 @@
+"""
+List of commonly accepted character names
+Key name always need to be properly capitalized and match in-game character name
+First index always need to match formal name but in all lowercase
+This make sure the profile command accepts a variation of valid character names
+"""
+common_names = {
+    "Kamisato Ayato": ["kamisato ayato", "ayato"],
+    "Kamisato Ayaka": ["kamisato ayaka", "ayaka", "ayaya"],
+    "Raiden Shogun": ["raiden shogun", "raiden", "shogun", "ei", "beelzebul"],
+    "Arataki Itto": ["arataki itto", "itto", "arataki"],
+    # Add more stuff here
+}
