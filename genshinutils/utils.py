f667cd213c901304a0a32f0419644e0613d9227e
Raiden <raidensakurajima@gmail.com>
2023-01-31 15:31:31 +0800

    Bump pre-commit versions, docs, format json

--- genshinutils/utils.py
+++ genshinutils/utils.py
@@ -10,65 +10,58 @@ from .constants import common_names
 
 log = logging.getLogger("red.raidensakura.genshinutils")
 
-"""I make it a function so it's more readable"""
-# Used internally
-def bytes_to_string(bytes):
-    str = bytes.decode()
-    return str
 
-
-"""I make it a function so it's more readable"""
-# Used internally
-def string_to_bytes(str):
-    bytes = str.encode()
-    return bytes
-
-
-"""
-Accepts: config
-Returns: str(encryption_key) or None
-"""
 # https://stackoverflow.com/questions/44432945/generating-own-key-with-python-fernet
 async def get_encryption_key(config):
-    key = string_to_bytes(await config.encryption_key())
+    """Fetch and convert encryption key from config
+
+    :param object config: Red V3 Config object
+    :return str: Plaintext encryption key
+    """
+    key = await config.encryption_key()
     if not key or key is None:
         key = Fernet.generate_key()
-        await config.encryption_key.set(bytes_to_string(key))
+        await config.encryption_key.set(key.decode())
+    else:
+        key = key.encode()
     return key
 
 
-"""
-Accepts: config, str(encoded)
-Returns: str(decoded)
-"""
-# decrypt config
 async def decrypt_config(config, encoded):
-    to_decode = string_to_bytes(encoded)
+    """Decrypt encrypted config data
+
+    :param object config: Red V3 Config object
+    :param str encoded: encoded data
+    :return str: decoded data
+    """
+    to_decode = encoded.encode()
     cipher_suite = Fernet(await get_encryption_key(config))
     decoded_bytes = cipher_suite.decrypt(to_decode)
-    decoded = bytes_to_string(decoded_bytes)
+    decoded = decoded_bytes.decode()
     return decoded
 
 
-"""
-Accepts: config, str(decoded)
-Returns: str(encoded)
-"""
-# encrypt config
 async def encrypt_config(config, decoded):
-    to_encode = string_to_bytes(decoded)
+    """Encrypt unencrypted data to store in config
+
+    :param object config: Red V3 Config object
+    :param str decoded: data to encrypt
+    :return str: encoded data
+    """
+    to_encode = decoded.encode()
     cipher_suite = Fernet(await get_encryption_key(config))
     encoded_bytes = cipher_suite.encrypt(to_encode)
-    encoded = bytes_to_string(encoded_bytes)
+    encoded = encoded_bytes.decode()
     return encoded
 
 
-"""
-Accepts: ( str(uid) | discord.Member ), self.config
-Returns: str(uid) or None
-"""
-# validate uid
 async def validate_uid(u, config):
+    """Return user UID from config or check if UID is valid
+
+    :param discord.Member or str u: User or UID to check
+    :param object config: Red V3 Config object
+    :return str: UID of the user if exist or valid
+    """
     if isinstance(u, discord.Member):
         uid = await config.user(u).UID()
         if uid:
@@ -88,34 +81,36 @@ async def validate_uid(u, config):
     return uid
 
 
-"""
-Accepts: str(name_query)
-Returns: str(formal_name) or None
-"""
-# validate_char_name
 def validate_char_name(arg):
+    """Validate character name against constants
+
+    :param str arg: name to check
+    :return str: Formal name of the character if exist
+    """
     formal_name = {i for i in common_names if arg in common_names[i]}
     if formal_name:
         return str(formal_name).strip("{'\"}")
 
 
-"""
-Accepts: str(uid), formal_name
-Returns: { UID: { Character: <Pillow Object> } }
-"""
-# enka_get_character_card
 async def enka_get_character_card(uid, char_name):
+    """Generate one or more character build image objects in a dict
+
+    :param str uid: UID of the player
+    :param str char_name: formal name of the character
+    :return dict: dict containing Pillow image object for the character
+    """
     async with encbanner.ENC(lang="en", splashArt=True, characterName=char_name) as encard:
         ENCpy = await encard.enc(uids=uid)
         return await encard.creat(ENCpy, 2)
 
 
-"""
-Accepts: config, discord.Member
-Returns: <Cookie Object>
-"""
-# get_user_cookie
 async def get_user_cookie(config, user):
+    """Retrieve user cookie from config
+
+    :param object config: Red V3 Config object
+    :param discord.Member user: Discord user to check for
+    :return cookie: Cookie object for the user
+    """
     ltuid_config = await config.user(user).ltuid()
     ltoken_config = await config.user(user).ltoken()
 
@@ -127,12 +122,14 @@ async def get_user_cookie(config, user):
     return cookie
 
 
-"""
-Accepts: str(title), str(desc), color
-Returns: discord.Embed
-"""
-# generate_embed
 def generate_embed(title="", desc="", color=""):
+    """Generate standardized Discord Embed usable for the whole cog
+
+    :param str title: Title of the embed, defaults to ""
+    :param str desc: Description of the embed, defaults to ""
+    :param str color: Color of the embed, defaults to ""
+    :return discord.Embed: Discord Embed object
+    """
     cog_url = "https://project-mei.xyz/genshinutils"
     e = discord.Embed(title=title, description=desc, color=color, url=cog_url)
     e.set_footer(
