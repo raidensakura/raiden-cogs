88305edfcbd589706ae671e8326ccb82f9f22027
Raiden Sakura <raiden@project-mei.xyz>
2025-07-27 16:24:19 +0800

    Remove unused import

--- choose/__init__.py
+++ choose/__init__.py
@@ -1,7 +1,5 @@
 import json
 from pathlib import Path
 
-from .choose import setup
-
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
